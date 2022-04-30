a8ab0a2214314907c81270b999c80503da8b34d8
Zarin Loosli <zarinloosli@gmail.com>
2022-04-30 14:01:07 -0600

    Render app shell

--- App.mjs
+++ App.mjs
@@ -1,5 +1,29 @@
 import htm from "https://unpkg.com/htm?module";
-import render from "./services/render.mjs";
+import { render, append } from "./services/render.mjs";
+
+const page = ""; // temporary, remove later
 
 const html = htm.bind(render);
-document.body.appendChild(html`<a href="/">Hello!</a>`);
+append(document.body, html`<header>
+  <button class="open-menu">
+    <span class="material-symbols-sharp"> menu </span>
+  </button>
+  <h1>${document.title}</h1>
+  <main>${page}</main>
+  <footer>
+    <nav>
+      <button id="view-contacts footer-button">
+        <span class="material-symbols-sharp footer-icon"> person </span>
+        <span class="footer-title">Contacts</span>
+      </button>
+      <button id="create-event footer-button fab">
+        <span class="material-symbols-sharp footer-icon"> add </span>
+      </button>
+      <button id="view-contacts footer-button">
+        <span class="material-symbols-sharp footer-icon"> person </span>
+        <span class="footer-title">Your invites</span>
+      </button>
+    </nav>
+  </footer>
+</header>
+`);
--- services/render.mjs
+++ services/render.mjs
@@ -1,8 +1,24 @@
-export default function render(type, props, ...children) {
+export function render(type, props, ...children) {
   const newEl = document.createElement(type);
   Object.assign(newEl, props);
   if (children) {
-    newEl.append(children);
+    children.forEach(child => {
+      newEl.append(child);
+    });
   }
   return newEl;
 }
+
+export function append(parent, element) {
+  console.log(element);
+  // if array of elements, recurse through array
+  if (typeof element === "array") {
+    element.forEach(el => {
+      append(el, parent);
+    });
+    return;
+  }
+
+  // Otherwise just append
+  parent.append(element);
+};
