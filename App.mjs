566840df565de9bd43ef7295aa6495a0b1f0bd89
Zarin Loosli <zarinloosli@gmail.com>
2022-04-30 14:22:58 -0600

    Add icons

--- App.mjs
+++ App.mjs
@@ -5,25 +5,25 @@ const page = ""; // temporary, remove later
 
 const html = htm.bind(render);
 append(document.body, html`<header>
-  <button class="open-menu">
-    <span class="material-symbols-sharp"> menu </span>
+  <button classList="open-menu">
+    <span classList=material-symbols-sharp> menu </span>
   </button>
   <h1>${document.title}</h1>
-  <main>${page}</main>
-  <footer>
-    <nav>
-      <button id="view-contacts footer-button">
-        <span class="material-symbols-sharp footer-icon"> person </span>
-        <span class="footer-title">Contacts</span>
-      </button>
-      <button id="create-event footer-button fab">
-        <span class="material-symbols-sharp footer-icon"> add </span>
-      </button>
-      <button id="view-contacts footer-button">
-        <span class="material-symbols-sharp footer-icon"> person </span>
-        <span class="footer-title">Your invites</span>
-      </button>
-    </nav>
-  </footer>
 </header>
+<main>${page}</main>
+<footer>
+  <nav>
+    <button id="view-contacts footer-button">
+      <span classList="material-symbols-sharp footer-icon"> person </span>
+      <span classList="footer-title">Contacts</span>
+    </button>
+    <button id="create-event footer-button fab">
+      <span classList="material-symbols-sharp footer-icon"> add </span>
+    </button>
+    <button id="view-contacts footer-button">
+      <span classList="material-symbols-sharp footer-icon"> person </span>
+      <span classList="footer-title">Your invites</span>
+    </button>
+  </nav>
+</footer>
 `);
--- services/render.mjs
+++ services/render.mjs
@@ -2,6 +2,7 @@ export function render(type, props, ...children) {
   const newEl = document.createElement(type);
   Object.assign(newEl, props);
   if (children) {
+    console.log(children);
     children.forEach(child => {
       newEl.append(child);
     });
@@ -10,12 +11,12 @@ export function render(type, props, ...children) {
 }
 
 export function append(parent, element) {
-  console.log(element);
   // if array of elements, recurse through array
-  if (typeof element === "array") {
-    element.forEach(el => {
-      append(el, parent);
-    });
+  if (element[0]) {
+    for (const el in element) {
+      console.log(element[el]);
+      append(parent, element[el]);
+    };
     return;
   }
 
