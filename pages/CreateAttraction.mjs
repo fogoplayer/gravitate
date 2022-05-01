5105b0480d4a7f8958149fbe2586b17674b81df9
Zarin Loosli <zarinloosli@gmail.com>
2022-04-30 23:19:33 -0600

    Add mapbox and center on user

--- pages/CreateAttraction.mjs
+++ pages/CreateAttraction.mjs
@@ -1,22 +1,38 @@
 import SelectInvitees from "../components/SelectInvitees.mjs";
 import TextInput from "../components/TextInput.mjs";
+import { MAPBOX_KEY } from "../services/config.mjs";
 import { html } from "../services/render.mjs";
 
+
 export default function CreateAttraction() {
-  return html`<form>
-  ${TextInput({
-    label: "Event Name",
-    id: "event-name",
-    name: "event-name",
+  return html`<div classList="ignore">
+  <div id="map"></div>
+  <script>
+    mapboxgl.accessToken = "${MAPBOX_KEY}";
+    navigator.geolocation.getCurrentPosition((position) => {
+      position = position.coords;
+      const map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: [position.longitude, position.latitude],
+        zoom:13,
+      });
+    });
+  </script>
+  <form>
+    ${TextInput({ label: "Event Name", id: "event-name", name: "event-name", })}
+    ${TextInput({
+    label: "Event Location", id: "event-location", name:
+      "event-location",
   })}
-  ${TextInput({
-    label: "Event Location",
-    id: "event-location",
-    name: "event-location",
-  })}
-  <button classList="flat" type="button">Use my location</button>
-  ${SelectInvitees()}
-  <button id="submit-button" classList="primary">Create attraction</button>
-</form >
-  `;
+    <button classList="flat" type="button">Use my location</button>
+    ${SelectInvitees()}
+    <button id="submit-button" classList="primary">Create attraction</button>
+  </form>
+</div>
+`;
 }
+async function onMapLoad() {
+  console.log(this);
+
+};
--- services/render.mjs
+++ services/render.mjs
@@ -5,7 +5,6 @@ export function render(type, props, ...children) {
   const newEl = document.createElement(type);
   Object.assign(newEl, props);
   if (children) {
-    console.log(children);
     children.forEach(child => {
       newEl.append(child);
     });
@@ -17,7 +16,6 @@ export function append(parent, element) {
   // if array of elements, recurse through array
   if (element[0]) {
     for (const el in element) {
-      console.log(element[el]);
       append(parent, element[el]);
     };
     return;
