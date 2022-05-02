101c00e2ae1644d8193c2fb446c8b942c156ff79
Zarin Loosli <zarinloosli@gmail.com>
2022-05-02 01:06:51 -0600

    Remove Contacts Page Contact

--- src/pages/Contacts.mjs
+++ src/pages/Contacts.mjs
@@ -1,9 +1,19 @@
 import ContactsList from "../components/ContactsList.mjs";
-import ContactsPageContact from "../components/ContactsPageContact.mjs";
-
+import { getDocData } from "../services/firebase/db.mjs";
+import { html } from "../services/render.mjs";
 
 export default function Contacts() {
-  return ContactsList(ContactsPageContact);
-
+  return ContactsList(Contact);
 }
 
+async function Contact(contacts) {
+  const jsx = html`<ul></ul>`;
+  contacts.forEach(async (contact) => {
+    if (contact.type === "document") {
+      contact = await getDocData(contact);
+    }
+    console.log(contact);
+    jsx.append(html`<li>${contact.name}</li>`);
+  });
+  return jsx;
+}
