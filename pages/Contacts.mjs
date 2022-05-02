21304c5439e36ab7cc5b49106cf726e554d4d43b
Zarin Loosli <zarinloosli@gmail.com>
2022-05-02 00:14:54 -0600

    Lazy-load contacts page

--- pages/Contacts.mjs
+++ pages/Contacts.mjs
@@ -1,65 +1,46 @@
 import ContactsList from "../components/ContactsList.mjs";
 import { getCurrentUser, getCurrUserData } from "../services/firebase.mjs";
-import { html } from "../services/render.mjs";
+import { append, html } from "../services/render.mjs";
 
-export default async function Contacts() {
+export default function Contacts() {
+  let page = html`<ul classList="interaction-list contacts-list">
+  <li classList="orbits-wrapper">
+    <h2>
+      <img src="./images/orbit.svg" alt="Orbit icon" classList="header-icon" />
+      <span classList="header-text">Orbits</span>
+    </h2>
+  </li>
+  <li classList="systems-wrapper">
+    <h2>
+      <img
+        src="./images/system.svg"
+        alt="Systems icon"
+        classList="header-icon"
+      />
+      <span classList="header-text">Systems</span>
+    </h2>
+  </li>
+  <li classList="friends-wrapper">
+    <h2>
+      <img
+        src="./images/friend.svg"
+        alt="Friends icon"
+        classList="header-icon"
+      />
+      <span classList="header-text">Friends</span>
+    </h2>
+  </li>
+</ul>
+`;
+  console.log(page);
+  populate(page);
+  return page;
+}
+
+async function populate(page) {
+  console.log(page);
   let { orbits, systems, friends, ...rest } = await getCurrUserData(await getCurrentUser());
-
-  return html`<ul classList="interaction-list contacts-list" >
-  <li classList="orbits-wrapper">
-    <h2>
-      <img src="./images/orbit.svg" alt="Orbit icon" classList="header-icon" />
-      <span classList="header-text">Orbits</span>
-    </h2>
-    ${html`${await ContactsList(orbits)}`}
-  </li >
-  <li classList="systems-wrapper">
-    <h2>
-      <img src="./images/system.svg" alt="Systems icon" classList="header-icon" />
-      <span classList="header-text">Systems</span>
-    </h2>
-    ${html`${await ContactsList(systems)}`}
-  </li>
-  <li classList="friends-wrapper">
-    <h2>
-      <img src="./images/friend.svg" alt="Friends icon" classList="header-icon" />
-      <span classList="header-text">Friends</span>
-    </h2>
-    ${html`${await ContactsList(friends)}`}
-  </li>
-</ul >
-  `;
-
-  return html`< ul classList = "interaction-list contacts-list" >
-  <li classList="orbits-wrapper">
-    <h2>
-      <img src="./images/orbit.svg" alt="Orbit icon" classList="header-icon" />
-      <span classList="header-text">Orbits</span>
-    </h2>
-    ${html`${await ContactsList(orbits)}`}
-  </li>
-  <li classList="systems-wrapper">
-    <h2>
-      <img src="./images/system.svg" alt="Systems icon" classList="header-icon" />
-      <span classList="header-text">Systems</span>
-    </h2>
-    ${html`${await ContactsList(systems)}`}
-    <ul classList="orbits">
-      <li>Friends</li>
-      <li>Family</li>
-      <li>School</li>
-    </ul>
-  </li>
-  <li classList="friends-wrapper">
-    <h2>
-      <img src="./images/friend.svg" alt="Friends icon" classList="header-icon" />
-      <span classList="header-text">Friends</span>
-    </h2>
-    <ul class="friends">
-      <li>John Doe</li>
-      <li>Sarah Jane</li>
-    </ul>
-  </li>
-</ul >
-  `;
-};;;;;;
+  ContactsList(orbits).then(contactsList => append(page.querySelector(".orbits-wrapper"), contactsList));
+  ContactsList(systems).then(contactsList => append(page.querySelector(".systems-wrapper"), contactsList));
+  ContactsList(friends).then(contactsList => append(page.querySelector(".friends-wrapper"), contactsList));
+}
