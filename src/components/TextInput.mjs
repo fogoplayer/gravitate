08d085b61a8489fa73792419c32f64180b69c4e3
Zarin Loosli <zarinloosli@gmail.com>
2022-05-03 22:23:56 -0600

    Fully hide input contents

--- src/components/TextInput.mjs
+++ src/components/TextInput.mjs
@@ -2,9 +2,12 @@ import { html } from "../services/render.mjs";
 
 export default function TextInput({ label, ref, oninput = () => { }, ...props }) {
 
-  const input = html`<label classList="text-input-wrapper"
-  ><span classList="text-input-label">${label}</span><input oninput=${onInput}classList="text-input" type="text" ...${props}
-/></label>
+  const input = html`<label classList="text-input-wrapper">
+    <span classList="text-input-label">${label}</span>
+    <div classList="curtain">
+      <input oninput=${onInput}classList="text-input" type="text" ...${props}/>
+    </div>
+  </label>
 `;
   onInput({ target: input.querySelector("input") });
   // Pass reference to object up to calling func
@@ -13,8 +16,8 @@ export default function TextInput({ label, ref, oninput = () => { }, ...props })
 
   function onInput(e) {
     const target = e.target;
-    if (target.value) target.parentNode.classList.add("not-empty");
-    else target.parentNode.classList.remove("not-empty");
+    if (target.value) target.parentNode.parentNode.classList.add("not-empty");
+    else target.parentNode.parentNode.classList.remove("not-empty");
     oninput(e);
   }
 }
