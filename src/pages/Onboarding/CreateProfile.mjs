2fed64867bf0a2c8a80582493a282c4a726945d3
Zarin Loosli <zarinloosli@gmail.com>
2022-05-07 23:26:14 -0600

    Design create profile page

--- src/pages/Onboarding/CreateProfile.mjs
+++ src/pages/Onboarding/CreateProfile.mjs
@@ -1,8 +1,24 @@
+import Input from "../../components/Input.mjs";
 import { jsx } from "../../services/render.mjs";
 
 export default function CreateProfile() {
   return jsx`<main classList="main-bubble">
-     <h1>Create Your Profile</h1>
-     
-  </main>`;
+  <form onsubmit=${onSubmit}>
+    <h1>Create your profile</h1>
+    ${Input({ label: "Username", id: "username" })}
+    <button classList="primary">Save</button>
+  </form>
+</main>
+`;
+}
+
+async function onSubmit(e) {
+  e.preventDefault();
+  try {
+    e.submitter.classList.add("loading");
+    await updateUsername(document.querySelector("#username").value);
+    page("view-attractions");
+  } catch (error) {
+    e.submitter.classList.remove("loading");
+  }
 }
