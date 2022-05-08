545b0068c0d3f4c7d94da15cad86e455a8f331d4
Zarin Loosli <zarinloosli@gmail.com>
2022-05-07 22:38:17 -0600

    Comment out messaging until we get it working

--- src/services/firebase/messaging.mjs
+++ src/services/firebase/messaging.mjs
@@ -4,20 +4,20 @@ import { getCurrUserData } from "./db.mjs";
 
 // Get registration token. Initially this makes a network call, once retrieved
 // subsequent calls to getToken will return from cache.
-const messaging = getMessaging();
-console.log(messaging);
-getToken(messaging, { vapidKey: 'BAl-Ec-obs63FVMmwQrp6QfF7v4DPojx99Ek5BBJMGuTxXFD6GiIYTwJI72JHNGgjIKhrp_NHJo3BdwQhngijhI' }).then((currentToken) => {
-  if (currentToken) {
-    update(getCurrUserData().ref, { messagingToken: currentToken });
-  } else {
-    // Show permission request UI
-    console.log('No registration token available. Request permission to generate one.');
-    // ...
-  }
-}).catch((err) => {
-  console.log('An error occurred while retrieving token. ', err);
-  // ...
-});
+// const messaging = getMessaging();
+// console.log(messaging);
+// getToken(messaging, { vapidKey: 'BAl-Ec-obs63FVMmwQrp6QfF7v4DPojx99Ek5BBJMGuTxXFD6GiIYTwJI72JHNGgjIKhrp_NHJo3BdwQhngijhI' }).then((currentToken) => {
+//   if (currentToken) {
+//     update(getCurrUserData().ref, { messagingToken: currentToken });
+//   } else {
+//     // Show permission request UI
+//     console.log('No registration token available. Request permission to generate one.');
+//     // ...
+//   }
+// }).catch((err) => {
+//   console.log('An error occurred while retrieving token. ', err);
+//   // ...
+// });
 
 // messaging.onMessageHandler = (message) => { console.log("Message received", message); };
 // messaging.onBackgroundMessageHandler = (message) => { console.log("BG Message received", message); };
