6c099d9032fd9226234696a6b5f89d4d263a2ec3
Zarin Loosli <zarinloosli@gmail.com>
2022-05-08 00:03:40 -0600

    NOTIFICATIONS ARE WORKING

---
+++ src/firebase-messaging-sw.js
@@ -0,0 +1,33 @@
+importScripts("https://www.gstatic.com/firebasejs/7.14.6/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/7.14.6/firebase-messaging.js"
+);
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDi2qWTznKlE8woT-ire6Sza5QEOQ6S4uY",
+  authDomain: "gravitate-43a9a.firebaseapp.com",
+  projectId: "gravitate-43a9a",
+  storageBucket: "gravitate-43a9a.appspot.com",
+  messagingSenderId: "143251031284",
+  appId: "1:143251031284:web:754f925a26adbf25d4caa1",
+  measurementId: "G-J60CGEV2MD",
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function (payload) {
+  console.log(payload);
+  const notification = JSON.parse(payload);
+  const notificationOption = {
+    body: notification.body,
+    icon: notification.icon,
+  };
+  return self.registration.showNotification(
+    payload.notification.title,
+    notificationOption
+  );
+});
--- src/services/firebase/messaging.mjs
+++ src/services/firebase/messaging.mjs
@@ -1,23 +1,35 @@
-import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-messaging.js";
+import {
+  getMessaging,
+  getToken,
+} from "https://www.gstatic.com/firebasejs/9.7.0/firebase-messaging.js";
+import { app } from "./app.mjs";
 import { update } from "./db.mjs";
 import { getCurrUserData } from "./db.mjs";
 
 // Get registration token. Initially this makes a network call, once retrieved
 // subsequent calls to getToken will return from cache.
-// const messaging = getMessaging();
-// console.log(messaging);
-// getToken(messaging, { vapidKey: 'BAl-Ec-obs63FVMmwQrp6QfF7v4DPojx99Ek5BBJMGuTxXFD6GiIYTwJI72JHNGgjIKhrp_NHJo3BdwQhngijhI' }).then((currentToken) => {
-//   if (currentToken) {
-//     update(getCurrUserData().ref, { messagingToken: currentToken });
-//   } else {
-//     // Show permission request UI
-//     console.log('No registration token available. Request permission to generate one.');
-//     // ...
-//   }
-// }).catch((err) => {
-//   console.log('An error occurred while retrieving token. ', err);
-//   // ...
-// });
+const messaging = getMessaging(app);
+console.log(messaging);
+getToken(messaging, {
+  vapidKey:
+    "BAl-Ec-obs63FVMmwQrp6QfF7v4DPojx99Ek5BBJMGuTxXFD6GiIYTwJI72JHNGgjIKhrp_NHJo3BdwQhngijhI",
+})
+  .then((currentToken) => {
+    if (currentToken) {
+      console.log(currentToken);
+      update(getCurrUserData().ref, { messagingToken: currentToken });
+    } else {
+      // Show permission request UI
+      console.log(
+        "No registration token available. Request permission to generate one."
+      );
+      // ...
+    }
+  })
+  .catch((err) => {
+    console.log("An error occurred while retrieving token. ", err);
+    // ...
+  });
 
 // messaging.onMessageHandler = (message) => { console.log("Message received", message); };
 // messaging.onBackgroundMessageHandler = (message) => { console.log("BG Message received", message); };
