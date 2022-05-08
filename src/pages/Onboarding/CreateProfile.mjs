0232fc3be82b7a2af7699bf9b9f755849a56186e
Zarin Loosli <zarinloosli@gmail.com>
2022-05-07 22:45:49 -0600

    Add create profile page

---
+++ src/pages/Onboarding/CreateProfile.mjs
@@ -0,0 +1,5 @@
+import { jsx } from "../../services/render.mjs";
+
+export default function CreateProfile() {
+  return jsx`<main>Testing</main>`;
+}
--- src/pages/Onboarding/index.js
+++ src/pages/Onboarding/index.js
@@ -1,6 +1,13 @@
 import { jsx } from "../../services/render.mjs";
+import CreateProfile from "./CreateProfile.mjs";
 
 export default function Onboarding(context) {
-  console.log(context);
-  return jsx`<div/>`;
+  switch (context.params.page) {
+    case "create-profile":
+      return CreateProfile();
+      break;
+
+    default:
+      break;
+  }
 }
