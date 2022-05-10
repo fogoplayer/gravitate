cfacdfadc1dc6d8875f9a080d1eece46a162c81d
Zarin Loosli <zarinloosli@gmail.com>
2022-05-10 17:06:32 -0600

    Store friends being added in a set

--- src/components/AddFriend.mjs
+++ src/components/AddFriend.mjs
@@ -4,11 +4,13 @@ import Input from "./Input.mjs";
 import Modal from "./Modal.mjs";
 
 export default function AddFriend() {
+  let newFriends = new Set();
+
   return Modal({
     contents: jsx`<form>
   <h1>Add a Friend</h1>
   <div classList="inline-inputs">
-    ${Input({ label: "Username", id: "friend-search-value" })}
+    ${Input({ label: "Username", id: "friend-search-value", required: true })}
     <button classList="flat inline" onclick="${searchForFriends}">
       <span classList="material-symbols-sharp">search</span>
     </button>
@@ -20,7 +22,7 @@ export default function AddFriend() {
 </aside>
 <form id="friend-search-results-container" classList="search-results empty">
 <ul id="friend-search-results" classList="search-results"></ul>
-<button disabled classList="primary" id="add-friends">Add friends</button>
+<button classList="primary" id="add-friends">Add friends</button>
 </form>`,
     id: "add-friend",
   });
@@ -40,26 +42,37 @@ export default function AddFriend() {
       .classList.remove("empty");
   }
 
+  async function addFriend(ref) {}
+
   function Template(user) {
     return jsx`<li>
   <label classList="contact-header-container">
-      <input
-        type="checkbox"
-        name="added-friends"
-        id="${user.name}"
-        value="${user.name}"
-        tabindex="0"
-      />
-      ${
-        (user.icon && user.icon[0]) === "/"
-          ? ""
-          : jsx`<span
-        classList="contact-icon"
-        >${user.icon || "🟣"}</span
-      >`
-      }<span classList="contact-name">${user.name}</span>
-    </label>
-    
+    <input
+      type="checkbox"
+      name="added-friends"
+      id="${user.name}"
+      value="${user.ref}"
+      onchange="${function (e) {
+        console.log(e.target.checked);
+        if (e.target.checked) {
+          newFriends.add(user.ref);
+        } else {
+          newFriends.delete(user.ref);
+        }
+        console.log(newFriends);
+      }}"
+      tabindex="0"
+      required
+    />
+    ${
+      (user.icon && user.icon[0]) === "/"
+        ? ""
+        : jsx`<span
+      classList="contact-icon"
+      >${user.icon || "🟣"}</span
+    >`
+    }<span classList="contact-name">${user.name}</span>
+  </label>
 </li>`;
   }
 }
