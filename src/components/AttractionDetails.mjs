b16cffffdbb53ef91e99007ae38e303d5ba19784
Zarin Loosli <zarinloosli@gmail.com>
2022-05-15 22:52:25 -0600

    Add guest lists and reactions to attraction details

--- src/components/AttractionDetails.mjs
+++ src/components/AttractionDetails.mjs
@@ -104,6 +104,45 @@ export function AttractionDetails(attraction) {
       <td>${attraction.expiration}</td>
     </tr>
   </table>
+  <h4>Invited</h4>
+  <table>
+    <tr>
+      <th>Orbits:</th>
+      <td>
+        ${attraction.orbits.map(
+          (orbit, index) => (index > 0 ? ", " : "") + orbit.name
+        )}
+      </td>
+    </tr>
+    <tr>    
+      <th>Systems:</th>
+      <td>
+        ${attraction.systems.map(
+          (system, index) => (index > 0 ? ", " : "") + system.name
+        )}
+      </td>
+    </tr>
+    <tr>
+      <th>Friends:</th>
+      <td>
+        ${attraction.friends.map(
+          (friend, index) => (index > 0 ? ", " : "") + friend.name
+        )}
+      </td>
+    </tr>
+    <tr>
+      <th>All invitees:</th>
+      <td>
+        ${attraction.guestList.map(
+          (guest, index) => (index > 0 ? ", " : "") + guest.name
+        )}
+      </td>
+    </tr>
+    <tr>
+      <th>Expiration:</th>
+      <td>${attraction.expiration}</td>
+    </tr>
+  </table>
   <h4>Responses</h4>
   <table>
     ${Object.keys(reactions).map((reaction) => {
