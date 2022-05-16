569f81e1d5b6bf304e0d887a052ce1aee9e15f1a
Zarin Loosli <zarinloosli@gmail.com>
2022-05-15 23:07:32 -0600

    Say when a category was not invited

--- src/components/AttractionDetails.mjs
+++ src/components/AttractionDetails.mjs
@@ -106,28 +106,40 @@ export function AttractionDetails(attraction) {
   </table>
   <h4>Invited</h4>
   <table>
-    <tr>
+      <tr>
       <th>Orbits:</th>
       <td>
-        ${attraction.orbits.map(
-          (orbit, index) => (index > 0 ? ", " : "") + orbit.name
-        )}
+        ${
+          (attraction.orbits.length &&
+            attraction.orbits.map(
+              (orbit, index) => (index > 0 ? ", " : "") + orbit.name
+            )) ||
+          jsx`<i>None</i>`
+        }
       </td>
     </tr>
     <tr>    
       <th>Systems:</th>
       <td>
-        ${attraction.systems.map(
-          (system, index) => (index > 0 ? ", " : "") + system.name
-        )}
+        ${
+          (attraction.systems.length &&
+            attraction.systems.map(
+              (system, index) => (index > 0 ? ", " : "") + system.name
+            )) ||
+          jsx`<i>None</i>`
+        }
       </td>
     </tr>
     <tr>
       <th>Friends:</th>
       <td>
-        ${attraction.friends.map(
-          (friend, index) => (index > 0 ? ", " : "") + friend.name
-        )}
+        ${
+          (attraction.orbits.length &&
+            attraction.friends.map(
+              (friend, index) => (index > 0 ? ", " : "") + friend.name
+            )) ||
+          jsx`<i>None</i>`
+        }
       </td>
     </tr>
     <tr>
