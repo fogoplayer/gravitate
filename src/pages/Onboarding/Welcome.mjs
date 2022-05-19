f7df9a61f1c4a77b282f49166bf75960a649c7f6
Zarin Loosli <zarinloosli@gmail.com>
2022-05-19 14:37:11 -0600

    Create welcome page

---
+++ src/pages/Onboarding/Welcome.mjs
@@ -0,0 +1,22 @@
+import { jsx } from "../../services/render.mjs";
+
+export function Welcome() {
+  return jsx`<main class="main-bubble modal">
+  <img src="../../images/cosmo.svg" class="cosmo-img" alt="Cosmo waving" />
+  <h1>Welcome to Gravitate!</h1>
+  <p>
+    Hi! My name's Cosmo, and I'd like to personally welcome you to Gravitate!
+  </p>
+  <p>
+    My job is to help you get started, and to show you the ropes around here. It
+    might feel like a lot, but I promise--you'll get the hang of it before too
+    long!
+  </p>
+  <p>
+    First, let's create a username and profile so that your friends can connect
+    with you!
+  </p>
+  <a class="button primary" href="create-profile">Continue</a>
+</main>
+`;
+}
--- src/pages/Onboarding/index.js
+++ src/pages/Onboarding/index.js
@@ -1,8 +1,13 @@
 import { jsx } from "../../services/render.mjs";
 import CreateProfile from "./CreateProfile.mjs";
+import { Welcome } from "./Welcome.mjs";
 
 export default function Onboarding(context) {
   switch (context.params.page) {
+    case "welcome":
+      return Welcome();
+      break;
+
     case "create-profile":
       return CreateProfile();
       break;
