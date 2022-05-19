bd42ab07333b1dd1e38fda01612ee74edcf51850
Zarin Loosli <zarinloosli@gmail.com>
2022-05-18 22:08:24 -0600

    Draft notification function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,29 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access Firestore.
+const admin = require("firebase-admin");
+admin.initializeApp();
+const db = admin.firestore();
+
+exports.sendInviteNotif = functions.firestore
+  .document("users/{uid}/invitations/{invitation}")
+  .onCreate(async (snap, context) => {
+    const uid = context.params.uid;
+    const invitation = context.params.invitation;
+
+    const userDoc = await functions.firestore.document("users/" + uid).get();
+    const userToken = userDoc.messagingToken;
+
+    const message = { data: userDoc, token: userToken };
+
+    getMessaging()
+      .send(message)
+      .then((response) => {
+        // Response is a message ID string.
+        console.log("Successfully sent message:", response);
+      })
+      .catch((error) => {
+        console.log("Error sending message:", error);
+      });
+  });
