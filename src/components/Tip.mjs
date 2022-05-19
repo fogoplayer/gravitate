0851727df81f9e905a42413d6a84d33137a3186e
Zarin Loosli <zarinloosli@gmail.com>
2022-05-19 16:14:40 -0600

    Basic tip styles

--- src/components/Tip.mjs
+++ src/components/Tip.mjs
@@ -1,8 +1,23 @@
-import { jsx } from "../services/render.mjs";
+import { jsx, renderPage } from "../services/render.mjs";
 
 export default function Tip({ target, contents, prev, next }) {
   return jsx`<dialog class="tip">
-  <img src="../../images/cosmo.svg" alt="Cosmo" />
-  ${contents}
-</dialog>`;
+  ${
+    prev
+      ? jsx`<button class="flat" onclick=${prev}>
+    <span class="material-symbols-sharp"> arrow_back</span>
+  </button>`
+      : ""
+  }
+  <section>
+    <img src="../../images/cosmo.svg" alt="Cosmo" />
+    ${contents}
+  </section>
+  <button class="flat" ...${
+    next ? { onclick: next } : { onclick: renderPage("/view-attractions") }
+  }>
+    <span class="material-symbols-sharp"> arrow_forward</span>
+  </button>
+</dialog>
+`;
 }
