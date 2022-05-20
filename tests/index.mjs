f88d3e891d653e3044ad69880d1631259d3f05c8
Zarin Loosli <zarinloosli@gmail.com>
2022-05-20 11:22:57 -0600

    Test that index loads

--- tests/index.mjs
+++
@@ -1,9 +0,0 @@
-import puppeteer from "puppeteer";
-
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("http://localhost:5000");
-
-  await browser.close();
-})();
