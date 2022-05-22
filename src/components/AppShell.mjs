f69d60527df96d294d54f903f10050a757c0af13
Zarin Loosli <zarinloosli@gmail.com>
2022-05-22 17:22:21 -0600

    Animate app shell

--- src/components/AppShell.mjs
+++ src/components/AppShell.mjs
@@ -5,6 +5,36 @@ import { jsx, renderPage } from "../services/render.mjs";
 export default function AppShell(hideAppDrawer, showAppDrawer, logOut) {
   return jsx`
 <div id="skip-to-content"><a href="#app-main" tabIndex=1>Skip to content</a></div>
+<dialog class="drop-nav" onclick=${(e) => {
+    if (e.target === document.querySelector(".drop-nav")) {
+      e.preventDefault();
+      hideAppDrawer();
+    }
+  }} tabindex=-1>
+ <nav>
+   <ul>
+     <li>
+       <a href="/changelog" class="nav-link" tabindex=0>
+         <span class="material-symbols-sharp">
+           list_alt</span>
+         <span class="nav-title">Changelog </span>
+       </a>
+     </li>
+     <li>
+       <a href="/settings" class="nav-link" tabindex=0>
+         <span class="material-symbols-sharp nav-icon">settings</span>
+         <span class="nav-title">Settings</span>
+       </a>
+     </li>
+     <li>
+       <a href="" class="nav-link" tabindex=0 onclick=${logOut}>
+         <span class="material-symbols-sharp nav-icon">logout</span>
+         <span class="nav-title">Log out</span>
+       </a>
+     </li>
+   </ul>
+ </nav>
+</dialog>
 <header class="app-header">
   <h1 class="page-title">Gravitate</h1>
   <button id="refresh-page" tabIndex=3 onclick=${(e) => {
@@ -17,38 +47,7 @@ export default function AppShell(hideAppDrawer, showAppDrawer, logOut) {
    <img class="pfp" src="${
      getCurrUserData().icon
    }" alt="User Profile Picture" /> 
-   <dialog class="drop-nav" onclick=${(e) => {
-     if (e.target === document.querySelector(".drop-nav")) {
-       e.preventDefault();
-       hideAppDrawer();
-     }
-   }} tabindex=-1>
-    <nav>
-      <ul>
-        <li>
-          <a href="/changelog" class="nav-link" tabindex=0>
-            <span class="material-symbols-sharp">
-              list_alt</span>
-            <span class="nav-title">Changelog </span>
-          </a>
-        </li>
-        <li>
-          <a href="/settings" class="nav-link" tabindex=0>
-            <span class="material-symbols-sharp nav-icon">settings</span>
-            <span class="nav-title">Settings</span>
-          </a>
-        </li>
-        <li>
-          <a href="" class="nav-link" tabindex=0 onclick=${logOut}>
-            <span class="material-symbols-sharp nav-icon">logout</span>
-            <span class="nav-title">Log out</span>
-          </a>
-        </li>
-      </ul>
-    </nav>
-  </dialog>
-
-  </button>
+   </button>
 </header>
 <main id="app-main" class="app-main"></main>
 <footer class="app-footer">
