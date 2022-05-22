8328b5ecde852ec1bfd82cadde656f428c407744
Zarin Loosli <zarinloosli@gmail.com>
2022-05-21 23:45:26 -0600

    Test create profile

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,6 +2,7 @@ const { contactsTest } = require("./contacts.mjs");
 const { createAttractionTest } = require("./create-attractions.mjs");
 const { driver } = require("./driver.mjs");
 const { loginTest } = require("./login.mjs");
+const { createProfileTest } = require("./onboarding/createProfile.mjs");
 const { tourTest } = require("./onboarding/tour.mjs");
 const { signupTest } = require("./signup.mjs");
 const { viewAttractionsTest } = require("./view-attractions.mjs");
@@ -14,10 +15,11 @@ test("Driver starts", () => {
 
 signupTest();
 loginTest();
-// viewAttractionsTest();
-// createAttractionTest();
-// contactsTest();
+createProfileTest();
 tourTest();
+viewAttractionsTest();
+createAttractionTest();
+contactsTest();
 
 test("Driver stops", () => {
   return driver.quit();
