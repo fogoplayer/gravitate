22b87d3dab38fac79f0847e0f925a3c36b208363
Zarin Loosli <zarinloosli@gmail.com>
2022-05-21 23:40:28 -0600

    Test signup

--- tests/app.test.js
+++ tests/app.test.js
@@ -3,14 +3,16 @@ const { createAttractionTest } = require("./create-attractions.mjs");
 const { driver } = require("./driver.mjs");
 const { loginTest } = require("./login.mjs");
 const { tourTest } = require("./onboarding/tour.mjs");
+const { signupTest } = require("./signup.mjs");
 const { viewAttractionsTest } = require("./view-attractions.mjs");
 
 const ROOT_URL = "http://localhost:5000/";
 
 test("Driver starts", () => {
-  return driver; //.then((d) => (driver = d));
+  return driver;
 });
 
+signupTest();
 loginTest();
 // viewAttractionsTest();
 // createAttractionTest();
---
+++ tests/signup.mjs
@@ -0,0 +1,13 @@
+const { querySelectorWait, driver } = require("./driver.mjs");
+
+function signupTest() {
+  describe("Signup tests", () => {
+    test("/signup loads", async () => {
+      await driver.get("http://localhost:5000/signup");
+      let h1 = await querySelectorWait("h1");
+      expect(await h1.getText()).toContain("Gravitate");
+    });
+  });
+}
+
+exports.signupTest = signupTest;
