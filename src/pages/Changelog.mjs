c1dbeb4ee286b2942b68862e04ab6cc3e2017ea6
Zarin Loosli <zarinloosli@gmail.com>
2022-05-22 23:09:27 -0600

    Fix margin alignment

--- src/pages/Changelog.mjs
+++ src/pages/Changelog.mjs
@@ -4,33 +4,32 @@ import { jsx } from "../services/render.mjs";
 export default function Changelog() {
   setPageTitle("Changelog");
   return jsx`<div class="changelog">
-  <h2>Changelog</h2>
   <section>
-    <h3>Alpha Release</h3>
-    <h4>1.3.1</h4>
+    <h2>Alpha Release</h2>
+    <h3>1.3.1</h3>
     <ul>
       <li>Add Roboto Slab theme font</li>
     </ul>
-    <h4>1.3.0</h4>
+    <h3>1.3.0</h3>
     <ul>
       <li>Move navigation into button on profile picture.</li>
       <li>Fix tour on smaller screens</li> 
     </ul>
-    <h4>1.2.0</h4>
+    <h3>1.2.0</h3>
     <ul>
       <li>Add settings page</li>
     </ul>
-    <h4>1.1.1</h4>
+    <h3>1.1.1</h3>
     <ul>
       <li>Create an end-to-end testing suite to validate future releases</li>
       <li>Run tests when pull requests are opened</li>
     </ul>
-    <h4>1.1.0</h4>
+    <h3>1.1.0</h3>
     <ul>
       <li>Add changelog</li>
     </ul>
 
-    <h4>1.0.0</h4>
+    <h3>1.0.0</h3>
     <ul>
       <li>Alpha release</li>
     </ul>
