e09140c98d7f0009cb70a62c31ffc992ef386f72
Zarin Loosli <zarinloosli@gmail.com>
2022-05-22 22:18:38 -0600

    Make H1's Roboto Slab

--- src/components/AppShell.mjs
+++ src/components/AppShell.mjs
@@ -45,10 +45,6 @@ export default function AppShell(hideAppDrawer, showAppDrawer, logOut) {
 </dialog>
 <header class="app-header">
 <h1 class="page-title">Gravitate</h1>
-<h1 class="page-title" style="font-family: 'Playfair Display';font-weight: 700;">Gravitate</h1>
-<h1 class="page-title" style="font-family: 'Roboto Slab'; font-weight: 100;">Gravitate</h1>
-<h1 class="page-title" style="font-family: 'Roboto Slab'; font-weight: 400;">Gravitate</h1>
-<h1 class="page-title" style="font-family: 'Roboto Slab'; font-weight: 700;">Gravitate</h1>
   <button id="refresh-page" tabIndex=3 onclick=${(e) => {
     renderPage(window.location.pathname);
     hideRefreshPage();
