6a854d7c2caf427774c4724819a79fcc15bb0555
Zarin Loosli <zarinloosli@gmail.com>
2022-07-27 22:13:29 -0600

    Update changelog

--- src/pages/Changelog.mjs
+++ src/pages/Changelog.mjs
@@ -11,6 +11,7 @@ export default function Changelog() {
         <li>Prepare for beta release</li>
         <li>Explain required permissions in tour</li>
         <li>Require a friend to be selected in Add Friend popup</li>
+        <li>Compress profile pictures on upload</li>
       </ul>
       <h3>1.8.0 <span class="date">7/26/22</span></h3>
       <ul>
